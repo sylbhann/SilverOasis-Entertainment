62d6b606049e2bdb75c0a10821aca44afb5d3cab
sylbhann <sylvan.grant@gmail.com>
2014-12-17 16:27:04 -0700

    Updated database
    Removed critical information

--- com.web.models/database.php
+++ com.web.models/database.php
@@ -1,9 +1,9 @@
 <?php
-$server ='classofcit.db.9575166.hostedresource.com';  // This will be localhost for you.   
-$dbname = 'classofcit';  //This will be the name of the database you create for your website. Create the database in phpmyadmin first. Mine had to have my username and a _ before the name of the database.   
+$server ='XXX.db.9575166.hostedresource.com';  // This will be localhost for you.   
+$dbname = 'XXX';  //This will be the name of the database you create for your website. Create the database in phpmyadmin first. Mine had to have my username and a _ before the name of the database.   
 $dsn = 'mysql:host='.$server.';dbname='.$dbname;  //here is where your server and database connect to each other  
-$username = 'classofcit';     // This will be the username for your hosting account.   
-$password = 'Caishe99!!';     // This will be the password you use for your hosting account.   
+$username = 'XXX';     // This will be the username for your hosting account.   
+$password = 'XXX';     // This will be the password you use for your hosting account.   
  
     try {
         $db = new PDO($dsn, $username, $password); // This trys to link your password and user name to your database 
