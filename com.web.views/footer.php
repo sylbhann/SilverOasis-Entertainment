5c58ec3636729c6623352632e7ae8831daa72dd4
Sterling <sterling@silveroasis.us>
2014-12-17 19:17:39 -0700

    Updated Footer and did CSS/HTML5 validations

--- com.web.views/footer.php
+++ com.web.views/footer.php
@@ -14,6 +14,14 @@ $date = date('Y')
                         <a href="/index.php?action=home" title="Home">SilverOasis.US Entertainment</a> &copy; <?php echo $date ?>. all rights reserved.
                         <br /><br />
                         <a href="/index.php?action=siteplan" title="Site Plan">Site Plan</a> | <a href="/index.php?action=presentation" title="Presentations">Presentation</a>
+                        <br /><br />
+                        
+                        <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                            <img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" height="31" width="88" title="Valid CSS!" />
+                        </a>
+                        <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fsilveroasis.us%2F&charset=%28detect+automatically%29&doctype=Inline&group=0&user-agent=W3C_Validator%2F1.3+http%3A%2F%2Fvalidator.w3.org%2Fservices#congrats" title="HTML5 Validator">
+                            <img src="http://www.w3.org/QA/Tools/I_heart_validator_lg" alt="Validators Donation Program logo" height="31" width="88" title="HTML5 Validation">
+                        </a>
                     </p>
                 </div>
             </footer>
